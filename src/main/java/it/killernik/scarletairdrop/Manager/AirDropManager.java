4f5c3e7797c8d144b1ca16a5e4a10dd779b0878f
killernikmaxi <103605100+killernik@users.noreply.github.com>
2024-04-20 15:39:44 +0200

    Fixati dei problemi per cui spawnavano piu' airdrop e item del dovuto + modifiche per permettere l'utilizzo di workload

--- src/main/java/it/killernik/scarletairdrop/Manager/AirDropManager.java
+++ src/main/java/it/killernik/scarletairdrop/Manager/AirDropManager.java
@@ -5,6 +5,8 @@ import it.killernik.scarletairdrop.Utils.BukkitSerialization;
 import it.killernik.scarletairdrop.Utils.HologramsUtils;
 import it.killernik.scarletairdrop.Utils.LocationUtils;
 import it.killernik.scarletairdrop.Utils.MessageUtil;
+import it.killernik.scarletairdrop.WorkLoad.Workload;
+import it.killernik.scarletairdrop.WorkLoad.impl.SpawnAirdropWorkload;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,6 +15,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class AirDropManager {
     public static List<Chest> chestList = new ArrayList<>();
@@ -20,55 +23,8 @@ public class AirDropManager {
     public HashMap<Chest, HologramsUtils> holoMap = new HashMap<>();
 
     public void startEvent() {
-
-        int maxADAmount = getMaxADAmount();
-        int minADAmount = getMinADAmount();
-        int airdropSpawned = 0;
-
-        List<String> locations = ScarletAirDrop.INSTANCE.getConfig().getStringList("Locations");
-        for (String locString : locations) {
-            locList.add(LocationUtils.stringToLocation(locString));
-        }
-
-        if (locList.isEmpty()) {
-            Bukkit.getLogger().warning("Evento fermato! Nessuna location impostata!");
-            return;
-        }
-
-        if (maxADAmount > locList.size()) {
-            maxADAmount = locList.size();
-        }
-
-        if (minADAmount > locList.size()) {
-            minADAmount = locList.size();
-        }
-
-        int range = maxADAmount - minADAmount + 1;
-        int AirDropAmount = (int) (Math.random() * range) + minADAmount;
-
-        for (Location loc : locList) {
-            spawnAirDrop(loc);
-            airdropSpawned++;
-            if (airdropSpawned == AirDropAmount) {
-                break;
-            }
-        }
-
-        Bukkit.broadcastMessage(MessageUtil.message(ScarletAirDrop.INSTANCE.getConfig().getString("Message.started").replaceAll("%airdrop%", String.valueOf(airdropSpawned))));
-
-        int airdropExpire = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.expire");
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-
-                for (Chest chest : chestList) {
-                    removeAirdrop(chest);
-                }
-
-            }
-        }.runTaskLater(ScarletAirDrop.INSTANCE, 20L * airdropExpire);
-
+        Workload spawnAirdropWorkload = new SpawnAirdropWorkload();
+        spawnAirdropWorkload.compute();
     }
 
     public void spawnAirDrop(Location loc) {
@@ -99,8 +55,8 @@ public class AirDropManager {
 
         int maxLootAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.Loots.max-item");
         int minLootAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.Loots.min-item");
-        int range = maxLootAmount - minLootAmount + 1;
-        int lootAmount = (int) (Math.random() * range) + minLootAmount;
+        Random rand = new Random();
+        int lootAmount = rand.nextInt(maxLootAmount - minLootAmount + 1) + minLootAmount;
         int lootSpawned = 0;
 
         List<String> items = ScarletAirDrop.INSTANCE.getConfig().getStringList("Loots");
@@ -116,12 +72,12 @@ public class AirDropManager {
 
     }
 
-    private int getMaxADAmount() {
+    public int getMaxADAmount() {
 
         int playersOnline = Bukkit.getOnlinePlayers().size();
         int maxADAmount;
 
-        if (playersOnline <= 7) {
+        if (playersOnline >= 7 && playersOnline < 15) {
             maxADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.7.max-airdrop");
             return maxADAmount;
         }
@@ -146,32 +102,32 @@ public class AirDropManager {
 
     }
 
-    private int getMinADAmount() {
+    public int getMinADAmount() {
 
         int playersOnline = Bukkit.getOnlinePlayers().size();
         int minADAmount;
 
         if (playersOnline >= 7 && playersOnline < 15) {
-            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.7.max-airdrop");
+            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.7.min-airdrop");
             return minADAmount;
         }
 
         if (playersOnline <= 15) {
-            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.15.max-airdrop");
+            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.15.min-airdrop");
             return minADAmount;
         }
 
         if (playersOnline <= 35) {
-            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.35.max-airdrop");
+            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.35.min-airdrop");
             return minADAmount;
         }
 
         if (playersOnline <= 50) {
-            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.50.max-airdrop");
+            minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.50.min-airdrop");
             return minADAmount;
         }
 
-        minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.max-on>50");
+        minADAmount = ScarletAirDrop.INSTANCE.getConfig().getInt("Settings.AirDrop.dynamic-airdrop-amount.min-on>50");
         return minADAmount;
 
     }
